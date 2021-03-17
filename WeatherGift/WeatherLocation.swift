426d7a4804de2e7f4d1185e5a400b11965f51ee6
Jay Jung <313jj2@gmail.com>
2021-03-16 20:48:48 -0400

    Section 6.2 – Toolbar and Locations List Table View

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Jay Jung on 3/16/21.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
