de2d9a364c4e7da80b48e55dfd4425bc9644af94
Jay Jung <313jj2@gmail.com>
2021-03-21 11:57:26 -0400

    Section 6.6 – Beginning the Location Detail UI

---
+++ WeatherGift/LocationDetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LocationDetailViewController.swift
+//  WeatherGift
+//
+//  Created by Jay Jung on 3/21/21.
+//
+
+import UIKit
+
+class LocationDetailViewController: UIViewController {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var placeLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var weatherLocation: WeatherLocation!
+    var weatherLocations: [WeatherLocation] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if weatherLocation == nil {
+            weatherLocation = WeatherLocation(name: "Current Location", latitude: 0.0, longitude: 0.0)
+            weatherLocations.append(weatherLocation)
+        }
+        
+        updateUserInterface()
+    }
+    
+    func updateUserInterface() {
+        dateLabel.text = ""
+        placeLabel.text = weatherLocation.name
+        temperatureLabel.text = "--°"
+        summaryLabel.text = ""
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! LocationListViewController
+        destination.weatherLocations = weatherLocations
+    }
+    
+    @IBAction func unwindFromLocationListViewController(segue: UIStoryboardSegue) {
+        let source = segue.source as! LocationListViewController
+        weatherLocations = source.weatherLocations
+        weatherLocation = weatherLocations[source.selectedLocationIndex]
+        updateUserInterface()
+    }
+}
+
