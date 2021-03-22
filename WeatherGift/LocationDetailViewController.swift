f73840f07cbdcc7f3d3f4749e8ef9711b8da7f26
Jay Jung <313jj2@gmail.com>
2021-03-21 22:14:43 -0400

    Section 6.10 – Using URL Session for OpenWeather API JSON Data

--- WeatherGift/LocationDetailViewController.swift
+++ WeatherGift/LocationDetailViewController.swift
@@ -36,6 +36,8 @@ class LocationDetailViewController: UIViewController {
         
         pageControl.numberOfPages = pageViewController.weatherLocations.count
         pageControl.currentPage = locationIndex
+        
+        weatherLocation.getData()
     }
     
     
