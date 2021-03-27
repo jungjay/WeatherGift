893dc6dae016cd395c5ca38f4b3762e113df5360
Jay Jung <313jj2@gmail.com>
2021-03-27 14:48:11 -0400

    Section 6.15 – Daily Weather in Custom TableView Cells; Using UITextView to Mimic UILabel

--- WeatherGift/LocationDetailViewController.swift
+++ WeatherGift/LocationDetailViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 private let dateFormatter: DateFormatter = {
     print("📆 I JUST CREATED A DATE FORMATTER!")
     let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "EEEE, MMM d, h:mm aaa"
+    dateFormatter.dateFormat = "EEEE, MMM d"
     return dateFormatter
 }()
 
@@ -21,13 +21,27 @@ class LocationDetailViewController: UIViewController {
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var tableView: UITableView!
     
     var weatherDetail: WeatherDetail!
     var locationIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        clearUserInterface()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         updateUserInterface()
+    }
+    
+    func clearUserInterface() {
+        dateLabel.text = ""
+        placeLabel.text = ""
+        temperatureLabel.text = ""
+        summaryLabel.text = ""
+        imageView.image = UIImage()
     }
 
     
@@ -52,6 +66,7 @@ class LocationDetailViewController: UIViewController {
                 self.temperatureLabel.text = "\(self.weatherDetail.temperature)°"
                 self.summaryLabel.text = self.weatherDetail.summary
                 self.imageView.image = UIImage(named: self.weatherDetail.dayIcon)
+                self.tableView.reloadData()
             }
         }
     }
@@ -83,3 +98,18 @@ class LocationDetailViewController: UIViewController {
     }
 }
 
+extension LocationDetailViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weatherDetail.dailyWeatherData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! DailyTableViewCell
+        cell.dailyWeather = weatherDetail.dailyWeatherData[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+}
