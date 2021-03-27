3027aa68aef80045b3b971b52ed2c96c2f742145
Jay Jung <313jj2@gmail.com>
2021-03-27 11:42:21 -0400

    Section 6.13 – Apple's TimeInterval, Date, DateFormatter, Time Zones

--- WeatherGift/LocationDetailViewController.swift
+++ WeatherGift/LocationDetailViewController.swift
@@ -7,6 +7,13 @@
 
 import UIKit
 
+private let dateFormatter: DateFormatter = {
+    print("📆 I JUST CREATED A DATE FORMATTER!")
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "EEEE, MMM d, h:mm aaa"
+    return dateFormatter
+}()
+
 class LocationDetailViewController: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var placeLabel: UILabel!
@@ -38,7 +45,9 @@ class LocationDetailViewController: UIViewController {
         
         weatherDetail.getData {
             DispatchQueue.main.async {
-                self.dateLabel.text = self.weatherDetail.timezone
+                dateFormatter.timeZone = TimeZone(identifier: self.weatherDetail.timezone)
+                let usableDate = Date(timeIntervalSince1970: self.weatherDetail.currentTime)
+                self.dateLabel.text = dateFormatter.string(from: usableDate)
                 self.placeLabel.text = self.weatherDetail.name
                 self.temperatureLabel.text = "\(self.weatherDetail.temperature)°"
                 self.summaryLabel.text = self.weatherDetail.summary
